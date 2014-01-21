a90061ab62512d0dc8effaf8d8565d021fba41ef
Abbas <abbas.sadat@gmail.com>
2014-01-20 20:34:35 -0800

    fixed the weired atrifacts.

--- src/jpeg.cpp
+++ src/jpeg.cpp
@@ -16,9 +16,9 @@ CVD::Image<CVD::Rgb<CVD::byte> > tempImg;
 CVD::Image<CVD::Rgb<CVD::byte> > orgImg_;
 CVD::Image<CVD::Rgb<CVD::byte> > orgImg;
 
-Matrix<8> T8;
+Matrix<8,8,double> T8;
 
-Matrix<8,8> quantizers = Data (
+Matrix<8,8,double> quantizers = Data (
             16, 11, 10, 16, 24, 40, 51, 61,
              12, 12, 14, 19, 26, 58, 60, 55,
              14, 13, 16, 24, 40, 57, 69, 56,
@@ -28,14 +28,24 @@ Matrix<8,8> quantizers = Data (
              49, 64, 78, 87, 103, 121, 120, 101,
              72, 92, 95, 98, 112, 100, 103, 99);
 
-Vector<3,int> RGB2YUV(CVD::Rgb<CVD::byte> rgb)
+Vector<3> RGB2YUV(CVD::Rgb<CVD::byte> rgb)
 {
     Vector<3> yuv;
-    Vector<3> rgbv = makeVector(rgb.red, rgb.green, rgb.blue);
+    Vector<3> rgbv = makeVector(rgb.red-128, rgb.green-128, rgb.blue-128);
     Matrix<3> m = Data(0.299, 0.587, 0.114,
                        -0.14713, -0.28886, 0.436,
                        0.615, -0.51499, -0.10001);
     return m*rgbv;
+}
+
+int cut(int a, int b)
+{
+    if(a<0)
+        return 0;
+    else
+        return a;
+
+    //return a<b?a:b;
 }
 
 CVD::Rgb<CVD::byte> YUV2RGB(Vector<3,double> yuv)
@@ -48,21 +58,21 @@ CVD::Rgb<CVD::byte> YUV2RGB(Vector<3,double> yuv)
                        1, 2.03211, 0);
 
      Vector<3> yuvv = m*yuv;
-     rgb.red = yuvv[0];
-     rgb.green = yuvv[1];
-     rgb.blue = yuvv[2];
+     rgb.red = cut(yuvv[0]+128,255);
+     rgb.green = cut(yuvv[1]+128,255);
+     rgb.blue = cut(yuvv[2]+128,255);
 
      return rgb;
 }
 
-void quantize(Matrix<8,8,int> &m)
+void quantize(Matrix<8,8,double> &m)
 {
     for(int i=0; i<8; i++)
         for(int j=0; j<8; j++)
-            m[i][j] = m[i][j]/ quantizers[i][j];
+            m[i][j] = ((m[i][j]/ (quantizers[i][j])));
 }
 
-void scale(Matrix<8,8,int> &m)
+void scale(Matrix<8,8,double> &m)
 {
     for(int i=0; i<8; i++)
         for(int j=0; j<8; j++)
@@ -104,7 +114,7 @@ void filter()
     for(int i=0; i<size/8; i++)
         for(int j=0; j<size/8; j++)
         {
-            Matrix<8,8,int> img8x8[3];
+            Matrix<8,8,double> img8x8[3];
             img8x8[0] = T8 * (img[0].slice(i*8,j*8,8,8 ) * T8.T());
             img8x8[1] = T8 * (img[1].slice(i*8,j*8,8,8 ) * T8.T());
             img8x8[2] = T8 * (img[2].slice(i*8,j*8,8,8 ) * T8.T());
@@ -121,7 +131,7 @@ void filter()
     for(int i=0; i<size/8; i++)
         for(int j=0; j<size/8; j++)
         {
-            Matrix<8,8,int> img8x8[3];
+            Matrix<8,8,double> img8x8[3];
             img8x8[0] = img[0].slice(i*8,j*8,8,8 );
             img8x8[1] = img[1].slice(i*8,j*8,8,8 );
             img8x8[2] = img[2].slice(i*8,j*8,8,8 );
@@ -168,9 +178,15 @@ void display()
 int main(int argc, char** argv) {
 
 
-    CVD::img_load(tempImg,  "neda.jpg" );
-    CVD::img_load(orgImg_,  "neda.jpg" );
-    CVD::img_load(orgImg,  "neda.jpg" );
+    if(argc < 2)
+    {
+        printf("please specify an image file!\n");
+        return 0;
+    }
+
+    CVD::img_load(tempImg,  argv[1] );
+    CVD::img_load(orgImg_,  argv[1] );
+    CVD::img_load(orgImg,   argv[1] );
 
     glutInit(&argc, argv);
 
